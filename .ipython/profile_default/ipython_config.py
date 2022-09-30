374e95c3e597191b894e3296d38f0e8de449ddd9
Connor Duffin <connor.p.duffin@gmail.com>
2022-09-30 15:40:09 +0100

    ipython config updated

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -630,3 +630,7 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+
+c.TerminalIPythonApp.extensions = [
+    'line_profiler',
+]
